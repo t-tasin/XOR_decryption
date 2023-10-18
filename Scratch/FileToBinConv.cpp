d27a3d8ec95d73fa7f588ff5af299138c8eb79b5
Leah Goodall <lgoodall@gm.slc.edu>
2023-10-18 12:17:46 -0400

    Finished the thing, it works now*

--- Scratch/FileToBinConv.cpp
+++ Scratch/FileToBinConv.cpp
@@ -7,10 +7,30 @@ using namespace std;
 class F2BConverter {
   public:
     string filepath;
+    bool isTXT;
+    bool is09;
+    char charList[11] = {'1','2','3','4','5','6','7','8','9','0'};
 
-  string convertFull(string fileName){
-    filepath = fileName;
-    string fileString = fileToString(fileName);
+    F2BConverter::F2BConverter(string file_path){
+      checkForTXT(file_path);
+      this->is09 = false;
+      this->filepath = file_path;
+    }
+
+  int checkForTXT(string filepath){
+    int len = filepath.length();
+    string txtCheck = filepath.substr(len-4);
+    if (txtCheck == ".txt"){
+      this->isTXT = true;
+    } else {
+      this->isTXT=false;
+      }
+    return 0;
+  }
+
+  string convertFull(){
+    filepath = this->filepath;
+    string fileString = fileToString();
     string bitString = strToBin(fileString);
     return bitString;
   }
@@ -18,28 +38,51 @@ class F2BConverter {
     string bitString;
     for (int i = 0; i < toConvert.length(); i++) { 
         string tString = bitset<8>(toConvert[i]).to_string();
-        bitString += tString;
+        if (tString == "11111111"){ // the ' ' is excluded here
+        //As we arent positive all txt files will have them, its easier to ignore when it shows up instead of looking for and removing it
+          ;
         }
+        else {
+          bitString += tString;
+        }
+      }
     return bitString;
   }
-  string fileToString(string fileName){
+  string fileToString(){
     string fileString;
-    ifstream file;
-    cout << fileName;
-    file.open(fileName);
+    fstream file;
+    file.open("testFile.txt");
     while(file){
-      cout << file;
-      /*fileString += file.get();
-      cout << fileString;*/
+      char c = file.get();
+      bool charCheck = false;
+      for (int i=0; i<10; i++){
+        if (c==this->charList[i] || bitset<8>(c).to_string()=="11111111"){ // get reads in ' 'with bit pattern 11111111
+          //this is ignored elsewhere so it just passes here
+          charCheck = true;
+          break;
+        }
       }
+      if (charCheck==false){
+        cout << "here: " << c << ' ' << bitset<8>(c).to_string() << "\n";
+        this->is09 = false;
+        break;
+      }else{
+        fileString += c;
+      }
+    }
+    this->is09 = true;
     file.close();
     return fileString;
+  }
+  bool beget(){
+    return this->is09;
   }
 };
 
 int main(){
-  F2BConverter conv;
-  string c = conv.convertFull("testFile.txt");
-  cout << c;
+  F2BConverter conv("testFile.txt");
+  string c = conv.convertFull();
+  cout << c << "\n";
+  cout << conv.beget();
   return 0;
 }
